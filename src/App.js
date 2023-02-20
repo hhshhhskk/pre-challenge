0f3c3f4f9f932c1151ac8fd3cc2d695b05b3ad15
hhshhhskk <hhshhhskk@naver.com>
2023-02-20 18:36:16 +0900

    페이지이동 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Side from "./components/Side";
+import Main from "./routes/Main";
+import PageA from "./routes/PageA";
+import PageB from "./routes/PageB";
+import PageC from "./routes/PageC";
+
+const Contents = styled.div`
+  display: flex;
+`;
+
+const Nav = styled.div`
+  margin-top: 10vh;
+  margin-left: 15vh;
+  width: 100%;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Contents>
+        <Side />
+        <Nav>
+          <Routes>
+            <Route path="/A" element={<PageA /> } />
+            <Route path="/B" element={<PageB /> } />
+            <Route path="/C" element={<PageC />} />
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Nav>
+      </Contents>
+    </>
   );
 }
 
